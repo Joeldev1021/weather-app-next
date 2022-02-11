7229d1d97619b32845f7e02657958faa21c1b6d2
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-11 16:00:44 -0500

    destruData and DestrudataToday

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,37 @@
 import type { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Banner from '../components/Banner'
+import CardDay from '../components/CardDay'
+import CardToday from '../components/CardToday'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Layout from '../components/Layout'
+import { destruData, destruDataToday } from '../helper/service'
 import styles from '../styles/Home.module.css'
+import { DayType, TodayProps } from '../type'
 
 interface Props {
   data : [] 
 }
 
 const Home: NextPage<Props> = ({data}) => {
-  console.log(data)
+   console.log(data) 
+   const res:DayType[] = destruData({data: data.consolidated_weather}) 
+   
+   const todayData:TodayProps[] = destruDataToday({data: data.consolidated_weather[0]})
+  console.log(res)
   return (
-    <div className={styles.container}>
-       
-    </div>
+    <Layout >
+      <Header /> 
+      <Banner temperature={res[0].the_temp} day={res[0].applicable_date} weatherName={res[0].weather_state_name}/>
+      <div className={styles['container-grid']}>
+        {res.map((item:DayType, index:number) =>  <CardDay index={index} key={item.id} dataDay={item}/>)}
+      </div> 
+     <div className={styles['grid-today']}>
+        <h2>{`Today's Hightlights`}</h2>
+        { todayData.map((item:TodayProps) => <CardToday item={item} key={item.name}/>) }
+     </div>
+     <Footer/>
+    </Layout>
   )
 }
 
@@ -20,7 +39,7 @@ export const getStaticProps:GetStaticProps<Props> = async () => {
   const store = await fetch("http://localhost:3000/api/weather") 
   const data = await store.json()
   return {
-    props: {
+     props: {
       data
     }
   }
