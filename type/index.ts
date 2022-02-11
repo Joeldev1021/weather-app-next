039178fa59d7f16de6da98c2e410e0233048cfb4
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-10 20:50:07 -0500

    interface DayType

---
+++ type/index.ts
@@ -0,0 +1,10 @@
+export interface DayType {
+    id: number,
+    the_temp: number,
+    max_temp: number,
+    min_temp: number,
+    aplicate_date: string,
+    whater_state_name: string,
+    applicable_date: string
+}
+
