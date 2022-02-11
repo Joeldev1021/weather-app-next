ba54b25aa92d6845238dbf116aed2fc3ea2361a2
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-10 19:03:35 -0500

    fetch data

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -5,7 +5,7 @@ type dataType = {
 }
 
 export default  function weatherApi(req: NextApiRequest, res: NextApiResponse) {
-    fetch("https://www.metaweather.com/api/location/search/?query=san")
+    fetch("https://www.metaweather.com/api/location/44418/")
      .then(response => response)
      .then(data => data.json())
      .then(data => {
