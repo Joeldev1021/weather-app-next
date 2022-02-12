b45ff96b825b7a5db9e8a23d87dd3ec7dcfdf081
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-12 12:14:58 -0500

    font famili Raleway monserrate

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head'
 import React from 'react'
 
@@ -10,6 +11,9 @@ const Layout = ({children}:Props) => {
     <div>
         <Head>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Raleway:wght@300;400;700&display=swap" rel="stylesheet"/>
         </Head>
         {children}
     </div>
