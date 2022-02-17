8fc7af009e40965cd3babdfeb01a93507ace48ad
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-17 17:45:01 -0500

    layout  data search

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-
+import Layout from '../components/Layout'
+import  App  from '../components/App'
 const Index = () => {
     const {id} = useRouter().query
-    console.log(id)
+    const [data, setData] = useState(null)
+    useEffect(() => {
+      setData(null)
+     fetch(`http://localhost:3000/api/weatherData/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
+    }, [id]) 
+    
   return (
-    <div>Search Country</div>
+    <Layout >
+      {data && <App data={data} />}
+    </Layout>
   )
 }
-
 
 
 export default Index
