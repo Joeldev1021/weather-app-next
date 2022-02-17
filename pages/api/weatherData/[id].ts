54e38f72eca2bf1ca841a005e8a57e940033f0ab
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-17 17:44:19 -0500

    api - getWeatherData

---
+++ pages/api/weatherData/[id].ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default function getWeatherData (req: NextApiRequest, res: NextApiResponse){
+    const {id } = req.query;
+    fetch(`https://www.metaweather.com/api/location/${id}`)
+     .then(response => response)
+     .then(data => data.json())
+     .then(data => {
+            res.json(data)
+        })
+     .catch(err => res.json(err))
+}
