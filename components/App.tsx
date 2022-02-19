620be4e00878ffa1c7146f67adc06c1d04d571d7
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-19 18:41:12 -0500

    App

--- components/App.tsx
+++ components/App.tsx
@@ -1,47 +1,58 @@
-import React, { useRef, useState } from 'react'
-import { destruData, destruDataToday } from '../helper/service'
-import { DayType, TodayInterface, TodayProps } from '../type'
-import Banner from './Banner'
-import CardDay from './CardDay'
-import CardToday from './CardToday'
-import Footer from './Footer'
-import Header from './Header'
-import styles from '../styles/Home.module.css'
-import ModalSearch from './ModalSearch'
+import React, { useRef, useState } from "react";
+import { destruData } from "../helper/service";
+import { DataWeather, DayType, TodayProps } from "../type";
+import Banner from "./Banner";
+import CardDay from "./CardDay";
+import CardToday from "./CardToday";
+import Footer from "./Footer";
+import styles from "../styles/Home.module.css";
+import ModalSearch from "./ModalSearch";
 
 interface Props {
-    data : []
+  data: any;
 }
 
-const App = ({data}:Props) => {
-   const res:DayType[] = destruData({data: data.consolidated_weather}) 
-   const resToday:TodayInterface = destruDataToday({data: data.consolidated_weather[0]})
+const App = ({ data}: Props) => {
+  const res: DataWeather = destruData({ data });
 
-   const [dataWeather, setDataWeather] = useState<DayType[]>(res)
-   const [dataToday, setDataToday] = useState(resToday)
-   const [showModal, setShowModal] = useState(false)
+  const [dataWeather, setDataWeather] = useState<DataWeather>(res);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
-   const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null);
 
-   const handleModal = () => {
-      ref.current?.classList.toggle(styles['show-modal'])
-   }
+  const handleModal = () => {
+    ref.current?.classList.toggle("show-modal");
+  };
+  
 
   return (
-    <>
-     <Header handleModal={handleModal}/> 
-      <ModalSearch ref={ref} handleModal={handleModal}/>
-      <Banner temperature={dataToday.the_temp} day={dataToday.applicable_date} weatherName={dataToday.weather_state_name} location={data.title}/>
-      <div className={styles['container-grid']}>
-         {dataWeather.map((item:DayType, index:number) =>  <CardDay index={index} key={item.id} dataDay={item}/>)}
-      </div> 
-      <h2 className={styles['sub-title']}>{`Today's Hightlights`}</h2>
-     <div className={styles['grid-today']}>
-        { dataToday.data.map((item:TodayProps) => <CardToday item={item} key={item.name}/>) }
-     </div>
-     <Footer/>
-    </>
-  )
-}
+    <div className={styles.App}>
+      <div className={styles["container-banner"]}>
+        <ModalSearch ref={ref} handleModal={handleModal} />
+        <Banner
+          handleModal={handleModal}
+          temperature={dataWeather.dataToday.the_temp}
+          day={dataWeather.dataToday.applicable_date}
+          weatherName={dataWeather.dataToday.weather_state_name}
+          location={dataWeather.title}
+        />
+      </div>
+      <div className={styles["section-grid"]}>
+        <div className={styles["container-grid"]}>
+          {dataWeather.dataDays.map((item: DayType, index: number) => (
+            <CardDay index={index} key={item.id} dataDay={item} />
+          ))}
+        </div>
+        <h2 className={styles["sub-title"]}>{`Today's Hightlights`}</h2>
+        <div className={styles["grid-today"]}>
+          {dataWeather.dataToday.data.map((item: TodayProps) => (
+            <CardToday item={item} key={item.name} />
+          ))}
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
