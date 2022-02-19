ce5e5f214585c904b78705d85d3c0383f6b0c771
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-19 18:37:32 -0500

    Cart-Today

--- components/CardToday.tsx
+++ components/CardToday.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import styles from "../styles/card-today.module.css"
+import { TodayProps } from '../type'
+import BarHumidity from './BarHumidity'
 
 interface Props {
-    item : {
-        name: string
-        value: number
-    }
+    item :TodayProps
 }
 
 const CardToday = ({item}:Props) => {
-    console.log(item)
+    const value = item.value.toString()
+    
   return (
         <div className={styles['card-today']}>
             <p className={styles.name}>{item.name}</p>
-            <p className={styles.value}>{item.value}</p>
+            <p className={styles.value}>{value.replace('.', ',')}<span className={styles.measure}>{item.measure}</span></p>
+            {item.name === "Humidity" ? <BarHumidity humidity={item.value}/> : null}
         </div>
   )
 }
