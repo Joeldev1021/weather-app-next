79f608a26cc74349f5725878a5286ff8dff503f7
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-19 18:38:54 -0500

    Card-day  todateString

---
+++ components/CardDay.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { DayType } from '../type'
+import styles from "../styles/card-day.module.css"
+import { imgWeather } from '../helper/service'
+
+interface Props {
+    dataDay: DayType;
+    index: number
+}
+
+
+const CardDay = ({dataDay, index}:Props) => {
+   const date = new Date(dataDay.applicable_date).toDateString().split(" ").slice(0, 3).join(" ")
+   const img = imgWeather[dataDay.weather_state_name.toLocaleLowerCase()] 
+  return (
+    <div className={styles['card-day']}>
+        <p key={dataDay.id}>{index == 0 ?"Tomorrow": date}</p>
+        <img className={styles['card-day-img']} src={img} alt="shower" />
+        <div className={styles['card-day-temp']}>
+            <p>{Math.round(dataDay.max_temp)} C</p>
+            <p>{Math.round(dataDay.min_temp)} C</p>
+        </div>
+    </div>
+  ) 
+}
+
+export default CardDay
