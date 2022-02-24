01edb3e8043ec443bbbfa8e4d98626288f520308
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-24 18:12:01 -0500

    link footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styles from '../styles/footer.module.css'
+import React from 'react';
+import styles from '../styles/footer.module.css';
 
 const Footer = () => {
-  return (
-    <div className={styles['footer']}>
-        <p>created by <span>username</span> - devChallenges.io</p>
-    </div>
-  )
-}
+    return (
+        <div className={styles['footer']}>
+            <p>created by <a href='https://devchallenges.io/'>username</a> - devChallenges.io</p>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
