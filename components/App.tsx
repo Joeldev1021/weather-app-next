4901f86e6305589e4c5496017ba3d4f48822722f
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-26 14:15:52 -0500

    isCentigrate

--- components/App.tsx
+++ components/App.tsx
@@ -8,7 +8,6 @@ import CardToday from "./CardToday";
 import Footer from "./Footer";
 import styles from "../styles/Home.module.css";
 import ModalSearch from "./ModalSearch";
-import Loading from "./Loading";
 
 interface Props {
   data: any;
@@ -16,22 +15,29 @@ interface Props {
 
 const App = ({ data}: Props) => {
     const res: DataWeather = destruData({ data });
-
     const [dataWeather, setDataWeather] = useState<DataWeather>(res);
+    const [isCentigrate, setIsCentigrate] = useState<boolean>(true);
 
     const ref = useRef<HTMLDivElement>(null);
 
     const handleModal = () => {
         ref.current?.classList.toggle("show-modal");
     };
-  
+
+    const handleCentigrade = ()=> {
+        setIsCentigrate(true); 
+    };
+
+    const handleFahrenheit = ()=> {
+        setIsCentigrate(false);
+    };
 
     return (
         <div className={styles.App}>
-            <Loading/>
             <div className={styles["container-banner"]}>
                 <ModalSearch ref={ref} handleModal={handleModal} />
                 <Banner
+                    isCentigrate={isCentigrate}
                     handleModal={handleModal}
                     temperature={dataWeather.dataToday.the_temp}
                     day={dataWeather.dataToday.applicable_date}
@@ -40,9 +46,13 @@ const App = ({ data}: Props) => {
                 />
             </div>
             <div className={styles["section-grid"]}>
+                <div className={styles["btn-group"]}>
+                    <button onClick={()=> handleCentigrade()}><span>o</span>C</button>
+                    <button onClick={()=> handleFahrenheit()}><span>o</span>F</button>
+                </div>
                 <div className={styles["container-grid"]}>
                     {dataWeather.dataDays.map((item: DayType, index: number) => (
-                        <CardDay index={index} key={item.id} dataDay={item} />
+                        <CardDay isCentigrate={isCentigrate} index={index} key={item.id} dataDay={item} />
                     ))}
                 </div>
                 <h2 className={styles["sub-title"]}>{`Today's Hightlights`}</h2>
