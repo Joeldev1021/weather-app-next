d4f200b9327d4b14c09e7fcc0de512af736efae9
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-26 14:17:37 -0500

    icon location-arrow

--- components/CardToday.tsx
+++ components/CardToday.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const CardToday = ({item}:Props) => {
     const value = item.value.toString();
-    
     return (
         <div className={styles['card-today']}>
             <p className={styles.name}>{item.name}</p>
             <p className={styles.value}>{value.replace('.', ',')}<span className={styles.measure}>{item.measure}</span></p>
             {item.name === "Humidity" ? <BarHumidity humidity={item.value}/> : null}
+            {item.name.includes("Wind") && <span className="fa-solid fa-location-arrow"></span>}
         </div>
     );
 };
