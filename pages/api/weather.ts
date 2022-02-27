8a88a4d3a406e0e55bf2bf703cfb5d3e49541636
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 08:06:29 -0500

    eslint api

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -6,11 +6,11 @@ type dataType = {
 
 export default  function weatherApi(req: NextApiRequest, res: NextApiResponse) {
     fetch("https://www.metaweather.com/api/location/44418/")
-     .then(response => response)
-     .then(data => data.json())
-     .then(data => {
-         res.json(data)
-     })     
+        .then(response => response)
+        .then(data => data.json())
+        .then(data => {
+            res.json(data);
+        });     
 
 }
 
