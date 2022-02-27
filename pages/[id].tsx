9923b07a9815f267728db8d26abb9e387b29606e
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 08:00:04 -0500

    query Id , loading state

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import Layout from '../components/Layout'
-import  App  from '../components/App'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Layout from '../components/Layout';
+import  App  from '../components/App';
+import Loading from '../components/Loading';
 const Index = () => {
 
-    const {id} = useRouter().query
+    const {id} = useRouter().query;
 
-    const [data, setData] = useState(null)
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-      setData(null)
-     fetch(`http://localhost:3000/api/weatherData/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      })
-    }, [id]) 
-    
-  return (
-    <Layout >
-      {data && <App data={data} />}
-    </Layout>
-  )
-}
+        setLoading(true);
+        setData(null);
+        fetch(`http://localhost:3000/api/weatherData/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+            })
+            .finally(() =>  setLoading(false));
+    }, [id]); 
+    return (
+        <Layout >
+            {loading ? <Loading/>: <App data={data} />} 
+        </Layout>
+    );
+};
 
 
-export default Index
+export default Index;
