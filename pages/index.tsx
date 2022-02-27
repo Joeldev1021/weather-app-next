2387f3a8bfac05ba38fd3b14a96ce8c6a649ae9d
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 08:20:28 -0500

    fetch api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
-import type { GetStaticProps, NextPage } from 'next';
+import type {  NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import App from '../components/App';
 import Layout from '../components/Layout';
 
-interface Props {
-  data : [] 
-}
-
-const Home: NextPage<Props> = ({data}) => {
+const Home: NextPage = () => {
+    const [data, setData] = useState([]); 
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/weather`)
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(err => console.log(err));
+    }, []);
+    
     return (
         <Layout >
             <App data={data} /> 
         </Layout>
     );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-    const response  = await fetch(`http://localhost:3000/api/weather`);
-    const data = await response.json();
-    return {
-        props: {
-            data
-        }
-    };
 };
 
 export default Home;
