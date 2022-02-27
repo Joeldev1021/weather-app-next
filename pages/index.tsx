2b15d312112dcd94293c84dfccbc046f5a28a118
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 07:59:10 -0500

    Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,27 @@
-import type { GetStaticProps, NextPage } from 'next'
-import { ParsedUrlQuery } from 'querystring'
-import App from '../components/App'
-import Layout from '../components/Layout'
+import type { GetStaticProps, NextPage } from 'next';
+import App from '../components/App';
+import Layout from '../components/Layout';
 
 interface Props {
   data : [] 
 }
 
-
 const Home: NextPage<Props> = ({data}) => {
-
-  return (
-    <Layout >
-      <App data={data} /> 
-    </Layout>
-  )
-}
+    return (
+        <Layout >
+            <App data={data} /> 
+        </Layout>
+    );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-    
-  const response  = await fetch(`http://localhost:3000/api/weather`)
-  const data = await response.json()
-  return {
-    props: {
-      data
-    }
-  }
-}
+    const response  = await fetch(`http://localhost:3000/api/weather`);
+    const data = await response.json();
+    return {
+        props: {
+            data
+        }
+    };
+};
 
-export default Home
+export default Home;
