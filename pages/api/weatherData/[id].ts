12f295e92e81686c91a124bbe68bc2682775df57
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 07:56:32 -0500

    search id location

--- pages/api/weatherData/[id].ts
+++ pages/api/weatherData/[id].ts
@@ -4,10 +4,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function getWeatherData (req: NextApiRequest, res: NextApiResponse){
     const {id } = req.query;
     fetch(`https://www.metaweather.com/api/location/${id}`)
-     .then(response => response)
-     .then(data => data.json())
-     .then(data => {
-            res.json(data)
+        .then(response => response)
+        .then(data => data.json())
+        .then(data => {
+            res.json(data);
         })
-     .catch(err => res.json(err))
+        .catch(err => res.json(err));
 }
