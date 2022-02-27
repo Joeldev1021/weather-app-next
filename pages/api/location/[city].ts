ddf59ae655d1d305ba4b1bcce675dc00b4c1411a
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 07:57:40 -0500

    get Id city

--- pages/api/location/[city].ts
+++ pages/api/location/[city].ts
@@ -1,17 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function getIdLocation(req: NextApiRequest, res: NextApiResponse) {
-   const {city }  = req.query 
-   fetch(`https://www.metaweather.com/api/location/search/?query=${city}`)
-    .then(response => response)
-    .then(data => data.json())
-    .then(data => {
-        res.json({data})
-    })
-    .catch(err => res.json(err))
+    const {city }  = req.query; 
+    fetch(`https://www.metaweather.com/api/location/search/?query=${city}`)
+        .then(response => response)
+        .then(data => data.json())
+        .then(data => {
+            res.json({data});
+        })
+        .catch(err => res.json(err));
 }
 
