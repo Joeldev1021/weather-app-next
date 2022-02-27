a5a850e717addb1f44d7ed3080afc32d6ebef799
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-27 08:02:12 -0500

    map suggestion, handleClick

--- components/ModalSearch.tsx
+++ components/ModalSearch.tsx
@@ -1,4 +1,4 @@
-/* import { useRouter } from 'next/router'; */
+import { useRouter } from 'next/router';
 import React, { forwardRef, Ref, useEffect, useState } from 'react';
 import styles from '../styles/modal-search.module.css';
 
@@ -18,17 +18,28 @@ type RefProps = Ref<HTMLDivElement>
 const ModalSearch = ({handleModal}:ModalProps, ref:RefProps) => {
     const [query, setQuery] = useState<string>('');
     const [searchQuery, setSearchQuery] = useState<typeSearch[]>([]);
-    /* const [cityId, setCityId] = useState<number>(); 
-       const route = useRouter();
- */
+    const [cityId, setCityId] = useState<number>(); 
+    const route = useRouter();
+ 
     const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setQuery(e.target.value);
     };   
-
+    // onsubmit
     const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        /*  getData(); */
+        getData(); 
     };
+    // onclick suggestion
+    const handleClick=(n:number)=> {
+        route.push(`/${n}`);
+    };
+    // get data
+    const getData = async () => {
+        const response = await fetch(`http://localhost:3000/api/location/${query}`);
+        const data = await response.json();
+        setCityId(data.data[0].woeid);
+    }; 
+
     useEffect(() => {
         if(query.length > 0){
             fetch(`http://localhost:3000/api/location/${query}`)
@@ -39,17 +50,12 @@ const ModalSearch = ({handleModal}:ModalProps, ref:RefProps) => {
         }
     }, [query]);
 
-    /* const getData = async () => {
-        const response = await fetch(`http://localhost:3000/api/location/${query}`);
-        const data = await response.json();
-        setCityId(data.data[0].woeid);
-    } 
     useEffect(() => {
         if(cityId) {
             route.push(`/${cityId}`);
         }
     }, [cityId, route]);
-  */  
+    
     return (
         <div ref={ref} className="modal-search">
             <button onClick={()=> handleModal()} className={styles['modal-close']}>x</button>
@@ -58,7 +64,7 @@ const ModalSearch = ({handleModal}:ModalProps, ref:RefProps) => {
                 <button>Search</button>
             </form>
             <div className={styles['container-suggestion']}>
-                {searchQuery.length> 0 &&  searchQuery.map(item => <p key={item.woeid}>{item.title}</p>)}
+                {searchQuery.length> 0 &&  searchQuery.map(item => <button onClick={()=> handleClick(item.woeid)} key={item.woeid}>{item.title}</button>)}
             </div>
         </div>
     );
