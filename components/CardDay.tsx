f91807d3a62c4a7611d89209c1964a6d7c00af62
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-03-06 16:39:53 -0500

    add Image next/Image

--- components/CardDay.tsx
+++ components/CardDay.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { DayType } from '../type';
 import styles from "../styles/card-day.module.css";
 import { centigrateToFarenheit, imgWeather } from '../helper/service';
+import Image from 'next/image';
 
 interface Props {
     dataDay: DayType;
@@ -34,7 +35,8 @@ const CardDay = ({dataDay, isCentigrate, index}:Props) => {
     return (
         <div className={styles['card-day']}>
             <p key={dataDay.id}>{index == 0 ?"Tomorrow": date}</p>
-            <img className={styles['card-day-img']} src={img} alt="shower" />
+            {/* <img className={styles['card-day-img']} src={img} alt="shower" /> */}
+            <Image height={50} width={50} src={img} alt={img}/>
             <div className={styles['card-day-temp']}>
                 <p>{maxTemp}<span className={styles.centigrate}>o</span>C</p>
                 <p>{minTemp}<span className={styles.centigrate}>o</span>C</p>
